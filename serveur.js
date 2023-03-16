bdda01a57564ec5be67a2bcc6690d624b0b4ae2d
AlexandreQuesnay <alexquesnay0803@gmail.com>
2023-03-16 10:42:47 +0100

    premier serveur node opérationel

--- serveur.js
+++ serveur.js
@@ -1,6 +1,9 @@
 let express=require('express');
 let app=express();
-let port = 3000;
+let port=3000;
+
+app.use(express.static(__dirname+'/www'));
+
 
 app.listen(port,()=>{
 console.log ('Le serveur est en route');
@@ -9,5 +12,5 @@ console.log (`Serveur listening at http://
 })
 
 app.get('/' ,(req,res,next)=>{
-res.send ('Bonjour !') ;
+res.sendFile ('/www/index.html') ;
 }) ;
